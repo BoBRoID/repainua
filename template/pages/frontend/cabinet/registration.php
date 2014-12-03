c8f400366335e3dfd8c44ff4cdf196fd92fceabb
BoBRoID <n.gilko@gmail.com>
2014-12-04 00:14:02 +0200

    some changes

--- template/pages/frontend/cabinet/registration.php
+++ template/pages/frontend/cabinet/registration.php
@@ -1,4 +1,20 @@
 <h1 class="pageHeader typicalBlock">Регистрация аккаунта</h1>
 <div class="marginTop typicalBlock">
     <h2>Шаг 1: данные пользователя</h2>
+    <hr class="silver">
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis arcu in dui convallis condimentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed ullamcorper tincidunt arcu. Integer ut neque faucibus, sagittis lacus vehicula, rutrum lectus. Vivamus sit amet lobortis nulla, eu porttitor nisl. Nulla et tortor non lorem tincidunt ullamcorper. Phasellus ut arcu nibh. Quisque et lectus quis nisi placerat sodales et et neque. Nulla eget justo mauris. Morbi ipsum erat, tincidunt sit amet sodales nec, dictum nec nisl.
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas viverra dignissim nibh. Quisque congue eros sed velit rhoncus rhoncus. Nullam id ante id nibh auctor feugiat. Fusce ultrices gravida massa id lobortis. Nunc bibendum lobortis lacus, a tempor erat ullamcorper eu. Ut convallis consequat velit, nec consectetur tellus semper eu. Donec euismod nec turpis vel ultrices. Sed sagittis purus sapien, ut placerat lacus fermentum non. Quisque eget rutrum quam. Sed quis pulvinar turpis. Morbi faucibus erat urna, vitae pretium magna congue sit amet. Praesent pellentesque consequat sodales. Vivamus vitae metus ut quam luctus accumsan sed id tellus.
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean quis arcu in dui convallis condimentum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed ullamcorper tincidunt arcu. Integer ut neque faucibus, sagittis lacus vehicula, rutrum lectus. Vivamus sit amet lobortis nulla, eu porttitor nisl. Nulla et tortor non lorem tincidunt ullamcorper. Phasellus ut arcu nibh. Quisque et lectus quis nisi placerat sodales et et neque. Nulla eget justo mauris. Morbi ipsum erat, tincidunt sit amet sodales nec, dictum nec nisl.
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam maximus erat eget libero interdum porttitor. Nullam scelerisque lectus sit amet faucibus ultricies. Suspendisse tristique libero id ante suscipit lacinia. Pellentesque tempus eros eget magna venenatis feugiat. Ut nec venenatis arcu. Quisque at imperdiet dolor. Sed viverra, diam sed pretium vulputate, purus nibh suscipit mauris, in fringilla ipsum tellus et mi. Sed at tempus neque, vel pharetra est. Vivamus at eros vestibulum, venenatis augue quis, pharetra diam. Pellentesque risus arcu, condimentum non elementum vitae, pretium in ex. Curabitur in orci sit amet felis consectetur efficitur vitae tristique arcu. Maecenas fringilla odio in egestas iaculis. Vestibulum vitae ligula convallis, ornare massa sed, rutrum velit. Nunc eget pellentesque metus, vitae consequat leo.
+    </p>
+    <p>
+        Mauris efficitur, nisl non vestibulum sodales, felis mi bibendum dolor, non molestie nulla ipsum sed massa. Sed ultrices ac diam a lacinia. Phasellus ultrices aliquet lacus sit amet accumsan. Morbi a suscipit nisi. Phasellus imperdiet pretium turpis, elementum tristique enim bibendum et. Sed rutrum sit amet mauris ut euismod. Etiam pharetra, dolor sit amet porta blandit, sem diam tristique justo, aliquam semper eros urna ut purus. Curabitur laoreet nulla magna. Ut consequat, tortor a consectetur ornare, tortor arcu sodales felis, quis convallis ex dolor a felis. Nunc ipsum velit, ultricies vel ante et, feugiat sollicitudin leo. Praesent commodo sollicitudin ex quis scelerisque. Vestibulum rhoncus aliquam magna, nec gravida sapien suscipit eu. Donec accumsan tempor sem, ac pellentesque urna blandit sed.
+    </p>
 </div>
